6987d62b6200ca6fb6b6748c0feec97b0c279d07
SantiagoB <santiagoborao@gmail.com>
2022-03-14 12:40:49 +0100

    Añadida la práctica 12

--- 11. Añadir Interacción js/script.js
+++ 11. Añadir Interacción js/script.js
@@ -5,6 +5,11 @@ AFRAME.registerComponent("change-color-on-gaze", {
   init() {
     this.originalColor = this.el.getAttribute("color");
     //this.originalColor = this.el.getAttribute('material').color;
+    
+    /*
+    * Los eventos mouseenter y mouseleave se dispararán cuando el cursor “se cruce” con la entidad
+    * que tenga un componente change-color-on-gaze y cuando “salga” de la entidad.
+    */
     this.el.addEventListener("mouseenter", () => {
       this.el.setAttribute("color", this.data.color);
     });
